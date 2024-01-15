c5107bf910d2dd807cb662528f0a63a8f5cc0820
Sergey Zolotykh <szolotykh88@gmail.com>
2024-01-15 00:42:35 -0800

    Updated setup file before release.

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 from setuptools import setup
+from setuptools import find_packages
 
 setup(name='pykinisi',
-      version='1.0.0',
+      version='0.0.1',
       description='Python package for Kinisi Controller',
       url='https://github.com/szolotykh/pykinisi',
       author='Sergey Zolotykh',
       author_email='szolotykh88@gmail.com',
       license='MIT',
-      packages=['pykinisi'],
-      zip_safe=False)
+      zip_safe=False,
+      packages=find_packages(),
+      install_requires=['serial'],
+      keywords=['motor controller', 'hardware', 'robotics', 'kinisi', 'kinisi controller'],
+      classifiers=['Development Status :: 3 - Alpha',
+                  'Programming Language :: Python :: 3',
+                  'License :: OSI Approved :: MIT License',
+                  'Operating System :: OS Independent'
+                   ]
+      )
