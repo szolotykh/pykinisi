e3e1f098a3a1d25d9aada4efe027f88f6999eca6
Sergey Zolotykh <szolotykh88@gmail.com>
2024-01-15 12:32:29 -0800

    Update version to 0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='pykinisi',
-      version='0.0.1',
+      version='0.0.2',
       description='Python package for Kinisi Controller',
       url='https://github.com/szolotykh/pykinisi',
       author='Sergey Zolotykh',
